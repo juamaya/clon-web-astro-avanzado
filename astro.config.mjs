62d718f06974565d7f7c72340add8db3ae4fd98c
juamaya <marinamaya@hotmail.es>
2024-05-16 18:08:30 +0200

    deploy 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://juamaya.github.io",
+  site: "https://juamaya.github.io/clon-web-astro-avanzado",
  
   integrations: [
     tailwind(),
