a05c1e55a227857142149bd52cb0b47b82300406
juamaya <marinamaya@hotmail.es>
2024-05-16 18:04:27 +0200

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:4321",
+  site: "https://juamaya.github.io",
  
   integrations: [
     tailwind(),
